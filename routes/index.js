b6ad2c0e6ec03d6a3ae229f2dc88353daba871e2
Parul Bhardwaj <bhardwaj.parul88@gmail.com>
2023-05-29 22:27:53 -0400

    Implemented responding to JSON

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,21 @@
 const Pet = require('../models/pet');
 
 module.exports = (app) => {
-
   /* GET home page. */
-  // app.get('/', (req, res) => {
-  //   Pet.find().exec((err, pets) => {
-  //     res.render('pets-index', { pets: pets });    
-  //   });
-  // });
+  // result.docs: the array of records on the current page
+  // result.page: the current page
+  //result.pages: the total number of pages
+  app.get('/', (req, res) => {
+    const page = req.query.page || 1
 
-/* GET home page. */
-// result.docs: the array of records on the current page
-// result.page: the current page
-//result.pages: the total number of pages
-app.get('/', (req, res) => {
-  const page = req.query.page || 1
-  Pet.paginate({}, {page: page}).then((results) => {
-    res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: results.page});     
+    Pet.paginate({}, { page: page }).then((results) => {
+      // If the request is JSON, we want to send a JSON response
+      if (req.header('Content-Type') == 'application/json') {
+        return res.json({ pets: results.docs, pagesCount: results.pages, currentPage: page });
+      // Otherwise we do what we did before
+      } else {
+        res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });
+      }
+    });
   });
-});
 }
